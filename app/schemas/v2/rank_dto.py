fd80b8013a10eec5f2246c1569f9ed075b8fb945
callein <callendev20@gmail.com>
2025-06-11 14:21:34 +0900

    :white_check_mark: feat: add RankDTOV2

---
+++ app/schemas/v2/rank_dto.py
@@ -0,0 +1,21 @@
+from typing import List, Optional
+
+from pydantic import BaseModel
+
+from app.schemas.v2.ad_element_extractor_dto import AdElementDTOV2
+from app.schemas.v2.search_dto import SearchDTOV2
+
+
+class RankDTOV2:
+    class GetRankPtfoRequest(BaseModel):
+        user_prompt: str
+        diversity: Optional[bool] = False
+
+        def to_ad_element_req_dto(self) -> AdElementDTOV2.AdElementRequest:
+            return AdElementDTOV2.AdElementRequest(
+                user_prompt=self.user_prompt,
+            )
+
+    class GetRankPtfoResponse(BaseModel):
+        generated: AdElementDTOV2.AdElementResponse
+        search_results: List[SearchDTOV2.SearchResponse]
