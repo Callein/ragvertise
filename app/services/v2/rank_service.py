dc8c16c5cad0a7cfd5eb8b2f5b248b644884d62a
callein <callendev20@gmail.com>
2025-06-11 21:25:49 +0900

    :white_check_mark: feat: add full factor to SearchDTOV2.SearchRequest - RankServiceV2.get_ranked_portfolios

--- app/services/v2/rank_service.py
+++ app/services/v2/rank_service.py
@@ -1,18 +1,24 @@
+from app.preprocess.text_cleaner import TextCleaner
 from app.schemas.v2.rank_dto import RankDTOV2
 from app.schemas.v2.search_dto import SearchDTOV2
-from app.services.v2.ad_element_extractor_service import AdElementExtractorServiceV2
+from app.services.v2.ad_element_extractor_service import ad_element_extractor_service_single_ton
 from app.services.v2.search_service import SearchServiceV2
 
 
 class RankServiceV2:
     def __init__(self):
         self.search_service = SearchServiceV2()
+        self.cleaner = TextCleaner()
 
     def get_ranked_portfolios(self, req: RankDTOV2.GetRankPtfoRequest) -> RankDTOV2.GetRankPtfoResponse:
         ad_element_req = req.to_ad_element_req_dto()
-        ad_element_resp = AdElementExtractorServiceV2.extract_elements(ad_element_req)
+        ad_element_resp = ad_element_extractor_service_single_ton.extract_elements(ad_element_req)
+
+        full_text = f"desc: {ad_element_resp.desc}. what: {ad_element_resp.what}. how: {ad_element_resp.how}. style: {ad_element_resp.style}"
+        clean_full_text = self.cleaner.clean(full_text)
 
         search_req = SearchDTOV2.SearchRequest(
+            full=clean_full_text,
             desc=ad_element_resp.desc,
             what=ad_element_resp.what,
             how=ad_element_resp.how,
