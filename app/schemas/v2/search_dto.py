34be5a5d81358fac036a27b575319cc5c66d70df
callein <callendev20@gmail.com>
2025-06-11 21:26:40 +0900

    :white_check_mark: feat: add fields to SearchDTOV2

--- app/schemas/v2/search_dto.py
+++ app/schemas/v2/search_dto.py
@@ -5,6 +5,7 @@ from pydantic import BaseModel
 
 class SearchDTOV2:
     class SearchRequest(BaseModel):
+        full: str
         desc: str
         what: str
         how: str
@@ -13,11 +14,16 @@ class SearchDTOV2:
 
     class SearchResponse(BaseModel):
         final_score: float
-        text_score: float
+        full_score: float
+        desc_score: float
         what_score: float
         how_score: float
         style_score: float
+        desc: str
+        what: str
+        how: str
+        style: str
         ptfo_seqno: int
         ptfo_nm: str
         ptfo_desc: str
-        tag_names: List[str]
+        tags: List[str]
