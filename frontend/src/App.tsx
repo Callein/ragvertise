147539e6b7d0c6e5499fcaca001fb532465ee336
callein <callendev20@gmail.com>
2025-08-07 00:45:29 +0900

    :white_check_mark: feat(front): set up main App component and route structure

---
+++ frontend/src/App.tsx
@@ -0,0 +1,20 @@
+import './main.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "@/pages/Home";
+import PromptPage from "@/pages/PromptPage";
+import RankResultPage from "@/pages/RankResultPage";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/prompt" element={<PromptPage />} />
+        <Route path="/rank-result" element={<RankResultPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
