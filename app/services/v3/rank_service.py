b840c9a0b05da9b29d712dcaba82113804818649
callein <callendev20@gmail.com>
2025-08-10 22:39:12 +0900

    :white_check_mark: be/feat(rank_service_v3): modify RankDTOV2 to V3 and SearchDTOV2 to V3

--- app/services/v3/rank_service.py
+++ app/services/v3/rank_service.py
@@ -1,7 +1,7 @@
 from app.preprocess.text_cleaner import TextCleaner
 from app.schemas.v2.ad_element_extractor_dto import AdElementDTOV2
-from app.schemas.v2.rank_dto import RankDTOV2
-from app.schemas.v2.search_dto import SearchDTOV2
+from app.schemas.v3.rank_dto import RankDTOV3
+from app.schemas.v3.search_dto import SearchDTOV3
 from app.services.v2.ad_element_extractor_service import ad_element_extractor_service_single_ton
 from app.services.v3.search_service import SearchServiceV3
 from app.utils.log_utils import get_logger
@@ -25,15 +25,15 @@ class RankServiceV3:
         self.search_service = SearchServiceV3()
         self.cleaner = TextCleaner()
 
-    def get_ranked_portfolios(self, req: RankDTOV2.GetRankPtfoRequest) -> RankDTOV2.GetRankPtfoResponse:
+    def get_ranked_portfolios(self, req: RankDTOV3.GetRankPtfoRequest) -> RankDTOV3.GetRankPtfoResponse:
         """
         주어진 요청(req)을 기반으로 광고 포트폴리오를 순위별로 반환.
 
         Args:
-            req (RankDTOV2.GetRankPtfoRequest): 사용자의 요청 데이터
+            req (RankDTOV3.GetRankPtfoRequest): 사용자의 요청 데이터
 
         Returns:
-            RankDTOV2.GetRankPtfoResponse: factor (desc, what, how, style) 및 랭킹 결과
+            RankDTOV3.GetRankPtfoResponse: factor (desc, what, how, style) 및 랭킹 결과
         """
         ad_element_req = req.to_ad_element_req_dto()
         ad_element_resp = ad_element_extractor_service_single_ton.extract_elements(ad_element_req)
@@ -41,16 +41,16 @@ class RankServiceV3:
         limit = self._validate_limit(req.limit)
         return self._rank_with_ad_elements(ad_element_resp, limit, req.diversity)
 
-    def get_ranked_portfolios_by_ad_elements(self, req: RankDTOV2.GetRankPtfoByAdElementsRequest) -> RankDTOV2.GetRankPtfoResponse:
+    def get_ranked_portfolios_by_ad_elements(self, req: RankDTOV3.GetRankPtfoByAdElementsRequest) -> RankDTOV3.GetRankPtfoResponse:
         """
         이미 추출된 광고 요소(ad_elements)를 기반으로 유사도 검사를 수행하고
         관련된 포트폴리오를 랭킹 순서로 반환합니다.
 
         Args:
-            req (RankDTOV2.GetRankPtfoByAdElementsRequest): 광고 요소 및 diversity 옵션이 포함된 요청 객체
+            req (RankDTOV3.GetRankPtfoByAdElementsRequest): 광고 요소 및 diversity 옵션이 포함된 요청 객체
 
         Returns:
-            RankDTOV2.GetRankPtfoResponse: 생성된 광고 요소와 랭킹된 포트폴리오 리스트
+            RankDTOV3.GetRankPtfoResponse: 생성된 광고 요소와 랭킹된 포트폴리오 리스트
         """
         ad_elements = req.to_ad_element_resp_dto()
         limit = self._validate_limit(req.limit)
@@ -74,21 +74,21 @@ class RankServiceV3:
             ad_element_resp: AdElementDTOV2.AdElementResponse,
             limit: int,
             diversity: bool
-    ) -> RankDTOV2.GetRankPtfoResponse:
+    ) -> RankDTOV3.GetRankPtfoResponse:
         """
         광고 요소(ad_element_resp)를 기반으로 포트폴리오를 검색하고 랭킹 결과를 반환합니다.
 
         Args:
-            ad_element_resp (RankDTOV2.AdElementResponse): 광고 요소 정보 (desc, what, how, style)
+            ad_element_resp (RankDTOV3.AdElementResponse): 광고 요소 정보 (desc, what, how, style)
             diversity (int): 검색 결과 다양성 조정 값
 
         Returns:
-            RankDTOV2.GetRankPtfoResponse: 광고 요소와 해당 요소 기반의 포트폴리오 랭킹 결과
+            RankDTOV3.GetRankPtfoResponse: 광고 요소와 해당 요소 기반의 포트폴리오 랭킹 결과
         """
         full_text = self._build_full_text(ad_element_resp)
         clean_full_text = self.cleaner.clean(full_text)
 
-        search_req = SearchDTOV2.SearchRequest(
+        search_req = SearchDTOV3.SearchRequest(
             full=clean_full_text,
             desc=ad_element_resp.desc,
             what=ad_element_resp.what,
@@ -100,7 +100,7 @@ class RankServiceV3:
 
         search_results = self.search_service.search(request=search_req)
 
-        return RankDTOV2.GetRankPtfoResponse(
+        return RankDTOV3.GetRankPtfoResponse(
             generated=ad_element_resp,
             search_results=search_results
         )
