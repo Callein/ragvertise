ba619656b2b1b6da4b52d226310e003e3e80a4aa
callein <callendev20@gmail.com>
2025-08-10 21:46:43 +0900

    🗃️ be/feat(ptfo_info): add metadata to PtfoInfo model

--- app/models/ptfo_info.py
+++ app/models/ptfo_info.py
@@ -1,5 +1,4 @@
 from sqlalchemy import Column, Integer, String
-
 from app.core.database import Base
 
 class PtfoInfo(Base):
@@ -7,3 +6,8 @@ class PtfoInfo(Base):
     PTFO_SEQNO = Column(Integer, primary_key=True, index=True)
     PTFO_NM = Column(String(50))
     PTFO_DESC = Column(String(1000))
+
+    VIEW_LNK_URL = Column(String(300))                              # 비디오 링크 URL
+    PRDN_STDO_NM = Column(String(100))                              # 업체명(제작스튜디오명)
+    PRDN_COST = Column(String(30))                                  # 제작비
+    PRDN_PERD      = Column(String(50))                             # 제작기간
